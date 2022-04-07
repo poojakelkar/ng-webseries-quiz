21818ff15fd5b9d59e9379a299fce8aaf68f45a9
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-07 13:45:28 +0530

    minor change

--- src/pages/Questions/index.jsx
+++ src/pages/Questions/index.jsx
@@ -1,33 +1,71 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { types } from "../../store/constants";
+import { QuizContext } from "../../store/context";
 import "./styles.css";
 
 const Questions = () => {
-    return (
-        <div className='question-container'>
-            <p className='title-text'>I don't play the odds, I play the man</p>
-            <div className='question-header'>
-                <p className='question1-point'>
-                    <span className='question-number'>Question:</span> 1 / 5
-                </p>
-                <p className='score'>
-                    <span className='question-score'>Score:</span> 0
-                </p>
-            </div>
-            <p className='question'>
-                Harvey Specture is considered one of New York's most brilliant
-                corporate litigation lawyers, but whats his unusual middle name?
-            </p>
-            <div className='options-btn'>
-                <button className='option-one options'>Michael</button>
-                <button className='option-two options'>Reginald</button>
-                <button className='option-three options'>Brandon</button>
-            </div>
-            <Link className='link' to='/answer'>
-                <button className='question-submit'>Submit</button>
-            </Link>
-        </div>
-    );
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(QuizContext);
+  const { quizId } = useParams();
+  const quiz = state?.quizzes.find((item) => item?.uuid === quizId);
+  const currentQuestion = quiz?.questions?.[state?.currentQuestionIndex];
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  const submitAnswerAndMoveToNextQuestion = () => {
+    const currentQuestionIndex = state?.currentQuestionIndex;
+    if (selectedAnswer) {
+      dispatch({
+        type: types.SUBMIT_ANSWER_AND_MOVE_TO_NEXT_QUESTION,
+        payload: {
+          uuid: quizId,
+          answer: selectedAnswer,
+        },
+      });
+      setSelectedAnswer("");
+    }
+    if (currentQuestionIndex >= quiz?.questions?.length - 1) {
+      navigate(`/quiz/${quizId}/result/`);
+    }
+  };
+
+  if (!quiz) {
+    return <div className="question-container">Quiz not found</div>;
+  }
+  return (
+    <div className="question-container">
+      <p className="title-text">{quiz?.title}</p>
+      <div className="question-header">
+        <p className="question1-point">
+          <span className="question-number">Question: </span>{" "}
+          {state?.currentQuestionIndex + 1} / {quiz?.questions?.length}
+        </p>
+        <p className="score">
+          <span className="question-score">Score: </span> 0
+        </p>
+      </div>
+      <p className="question">{currentQuestion?.question}</p>
+      <div className="options-btn">
+        {currentQuestion?.options?.map((opt, index) => (
+          <button
+            key={index}
+            className={`${
+              selectedAnswer === opt ? "selected-option" : ""
+            } options`}
+            onClick={() => setSelectedAnswer(opt)}
+          >
+            {opt}
+          </button>
+        ))}
+      </div>
+      <button
+        className="question-submit"
+        onClick={submitAnswerAndMoveToNextQuestion}
+      >
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default Questions;
