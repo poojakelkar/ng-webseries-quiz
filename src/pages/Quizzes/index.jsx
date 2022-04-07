1ff1aa43cb525e20f0ebd5d50e38c9c17493bd08
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-07 11:51:24 +0530

    added axios

--- src/pages/Quizzes/index.jsx
+++ src/pages/Quizzes/index.jsx
@@ -1,29 +1,45 @@
-import React, { useContext } from "react";
+import axios from "axios";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { types } from "../../store/constants";
 import { QuizContext } from "../../store/context";
 import "./styles.css";
 
 const Finsight = () => {
-  const { state } = useContext(QuizContext);
+  const { state, dispatch } = useContext(QuizContext);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "https://quiz-fake-db.aliarslanansari.repl.co/quizzes",
+    }).then((response) => {
+      dispatch({
+        type: types.STORE_QUIZZES,
+        payload: response?.data,
+      });
+      console.log({ response });
+    });
+  }, [dispatch]);
 
   return (
     <div className="grid-container">
-      {state?.quizzes?.map((quiz) => {
-        return (
-          <Link key={quiz?.uuid} to={`/quiz/${quiz?.uuid}`} className="link">
-            <div className="card-1">
-              <img
-                className="poster-1"
-                src={quiz.thumbnail}
-                alt="postr-1"
-              ></img>
-              <p className="img-label">{quiz.title}</p>
-              <p className="subtext">Take quiz to check yourself</p>
-              <p className="point">{quiz?.questions?.length} Questions</p>
-            </div>
-          </Link>
-        );
-      })}
+      {!!state?.quizzes?.length &&
+        state?.quizzes?.map((quiz) => {
+          return (
+            <Link key={quiz?.uuid} to={`/quiz/${quiz?.uuid}`} className="link">
+              <div className="card-1">
+                <img
+                  className="poster-1"
+                  src={quiz.thumbnail}
+                  alt="postr-1"
+                ></img>
+                <p className="img-label">{quiz.title}</p>
+                <p className="subtext">Take quiz to check yourself</p>
+                <p className="point">{quiz?.questions?.length} Questions</p>
+              </div>
+            </Link>
+          );
+        })}
     </div>
   );
 };
